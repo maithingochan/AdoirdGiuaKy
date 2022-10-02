27aa8fd2567cdbf5170cf3e2c56fba0d9b91b787
maithingochan <89406666+maithingochan@users.noreply.github.com>
2022-10-03 06:45:06 +0700

    Add files via upload

---
+++ Listview/app/src/main/java/com/example/custom_listview/MainActivity.java
@@ -0,0 +1,63 @@
+package com.example.custom_listview;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+
+
+import com.example.custom_listview.databinding.ActivityMainBinding;
+import com.example.custom_listview.databinding.ActivityUserBinding;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        int[] imageId = {R.drawable.a,R.drawable.b,R.drawable.c,R.drawable.d,R.drawable.e,
+                R.drawable.f,R.drawable.g,R.drawable.h,R.drawable.i};
+        String[] name = {"Thanh ha","Thanh Hoang", "Thanh Hieu", "Thanh Mong"};
+        String[] lastMessage = {"Let's Catchup","Dinner tonight?","Gotta go","any Weekend Plans?"};
+        String[] lastMsgTime = {"8:45 pm","9:00 am","7:34 pm","6:32 am"};
+        String[] phoneNo = {"7656610000","9999043232","7834354323","9876543211"};
+        String[] country = {"VietNamese","Russia","Germany","France"};;
+
+        ArrayList<User> userArrayList = new ArrayList<>();
+
+        for(int i = 0;i< imageId.length;i++){
+
+            User user = new User(name[i],lastMessage[i],lastmsgTime[i],phoneNo[i],country[i],imageId[i]);
+            userArrayList.add(user);
+
+        }
+
+
+        ListAdapter listAdapter = new ListAdapter(MainActivity.this,userArrayList);
+
+        binding.listview.setAdapter(listAdapter);
+        binding.listview.setClickable(true);
+        binding.listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent i = new Intent(MainActivity.this,UserActivity.class);
+                i.putExtra("name",name[position]);
+                i.putExtra("phone",phoneNo[position]);
+                i.putExtra("country",country[position]);
+                i.putExtra("imageid",imageId[position]);
+                startActivity(i);
+
+            }
+        });
+
+    }
+}
